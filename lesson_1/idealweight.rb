5008e861baddf1285a629dcbf28036bef1660069
malovevgen <malovevgen@yandex.ru>
2019-04-17 21:40:43 +0300

    изменения оформления файла

--- lesson_1/idealweight.rb
+++ lesson_1/idealweight.rb
@@ -5,18 +5,18 @@
 случаях сообщение содержит о необходимости коррекции веса.
 =end
 puts "Как Вас зовут?"
-name= gets.chomp.capitalize!
-puts "Какой у Вас вес,#{name}?"
-weight=gets.chomp.to_f
-puts "Какой у Вас рост,#{name}?"
-height=gets.chomp.to_f
-coef=height-weight
-idealweight=110.0
-maxdeviation=5.0
-if coef>idealweight+maxdeviation
-	puts "#{name},Ваш вес недостаточный. Вам надо набрать #{coef-idealweight} килограммов"
+name = gets.chomp.capitalize
+puts "Какой у Вас вес, # { name } ?"
+weight = gets.chomp.to_f
+puts "Какой у Вас рост, # { name } ?"
+height = gets.chomp.to_f
+coef = height - weight
+idealweight = 110.0
+maxdeviation = 5.0
+if coef > idealweight + maxdeviation
+  puts " # { name } , Ваш вес недостаточный. Вам надо набрать  # { coef - idealweight } килограммов"
 elsif coef<idealweight-maxdeviation
-	puts "#{name}, у Bаc лишний вес. Вам надо сбросить #{idealweight-coef} килограммов"
+  puts " # { name } , у Bаc лишний вес. Вам надо сбросить # { idealweight - coef } килограммов"
 else
-	puts "#{name},  Bаш вес идеально соответствует росту!)))"
+  puts " # { name },  Bаш вес идеально соответствует росту!)))"
  end
