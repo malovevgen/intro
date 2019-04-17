9e1f20122b426bec902e3544dd31dc65436d5281
malovevgen <malovevgen@yandex.ru>
2019-04-17 23:25:36 +0300

    code change

--- lesson_1/right_triangle.rb
+++ lesson_1/right_triangle.rb
@@ -8,11 +8,7 @@ puts "Введите длину третьей стороны треугольн
 c = gets.chomp.to_f
 
 a, b, c = [a, b, c].sort
-#sidearray = [a, b, c]
-#sidearray.sort!
-#a = sidearray[0]
-#b = sidearray[1]
-#c = sidearra
+
 puts "Треугольник прямоугольный" if c**2 == a**2 + b**2
 print "Треугольник равнобедренный" if (a == b) || (b == c) || (a == c) 
 puts " и равносторонний" if (a == b) && (b == c) && (a == c)
