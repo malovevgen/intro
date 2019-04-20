5c563aa2cf2b2c225b2b251eddc4ed1253acde68
malovevgen <malovevgen@yandex.ru>
2019-04-20 20:05:55 +0300

    load

---
+++ lesson_2/fibon.rb
@@ -0,0 +1,9 @@
+fibon = [0,1]
+numnext = 1
+last_num = 100
+loop do
+  fibon.push (numnext)
+  numnext = fibon [-1] + fibon [-2]
+  break if numnext > last_num
+end
+fibon.each {| value | puts value }
