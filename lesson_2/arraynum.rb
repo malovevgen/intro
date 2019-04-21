835abc95d34f7d635c33009bb14c2dcc39c2242a
malovevgen <malovevgen@yandex.ru>
2019-04-21 15:47:39 +0300

    fixed formatting

--- lesson_2/arraynum.rb
+++ lesson_2/arraynum.rb
@@ -3,8 +3,8 @@ num = 10
 step = 5
 last_num = 100
 loop do
- arraynum.push (num)
- num += step
- break if num > last_num
+  arraynum.push(num)
+  num += step
+  break if num > last_num
 end
-arraynum.each {| value | puts value }
+arraynum.each { |value| puts value }
