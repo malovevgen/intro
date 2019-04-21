65d22bfc1dc6bb93bb142a467a72aa71efbec0df
malovevgen <malovevgen@yandex.ru>
2019-04-21 18:03:05 +0300

    replace loop with while

--- lesson_2/fibon.rb
+++ lesson_2/fibon.rb
@@ -1,9 +1,6 @@
 fibon = [0, 1]
-numnext = 1
 last_num = 100
-loop do
+while (numnext = fibon[-1] + fibon[-2]) <= last_num do
   fibon.push(numnext)
-  numnext = fibon[-1] + fibon[-2]
-  break if numnext >= last_num
 end
-fibon.each { |value| puts value }
+puts fibon
