04160715de0aed96a64b68f048d100b196ad54d5
malovevgen <malovevgen@yandex.ru>
2019-04-22 20:51:48 +0300

    delete hash

--- lesson_2/vowels.rb
+++ lesson_2/vowels.rb
@@ -1,9 +1,6 @@
 alphabet = ("a".."z").zip(1..26).to_h
 hash_vowels = {}
 string_vowels = "aeiouy"
-alphabet.each do |letter, ind|
-  hash_vowels[letter] = ind if string_vowels.include? letter
-end
-hash_vowels.each do |letter, ind|
-  puts "#{letter}: #{ind}"
+alphabet.each do |letter, index|
+  puts "#{letter}: #{index}" if string_vowels.include? letter
 end
