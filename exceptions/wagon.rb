950108373f94cb73cdc921829bcd05ed568b78eb
malovevgen <malovevgen@yandex.ru>
2019-05-25 16:31:28 +0300

    load

--- exceptions/wagon.rb
+++ exceptions/wagon.rb
@@ -1,7 +1,23 @@
 class Wagon
   attr_accessor :number
 
+  NUMBER_format = /^\d{6}$/
+
   def initialize(number)
     @number = number
+    validate!
+  end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false
+  end
+
+  private
+
+  def validate!
+    raise if number !~ NUMBER_FORMAT
   end
 end
